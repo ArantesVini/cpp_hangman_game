06258e9db08bf4d7afe5e26f7a66152a8bf1e4bd
ArantesVini <vinicius.daniel02@gmail.com>
2023-09-03 20:40:43 -0300

    right guess function to check the char ocurrence in word

--- hangman_game/hangman_game.cpp
+++ hangman_game/hangman_game.cpp
@@ -3,10 +3,23 @@
 #include <ctime>
 using namespace std;
 
+const string SECRET_WORD = "MELANCIA";
+
+bool right_guess(char guess)
+{
+    for (char letter : SECRET_WORD)
+    {
+        if (guess == letter)
+            return true;
+        else
+            return false;
+    }
+    return false;
+}
+
 int main()
 {
 
-    const string SECRET_WORD = "MELANCIA";
     cout << SECRET_WORD << endl;
 
     bool hanged = false;
@@ -17,5 +30,14 @@ int main()
         char guess;
 
         cin >> guess;
+        guess = toupper(guess);
+        if (right_guess(guess))
+        {
+            cout << "You got it right!" << endl;
+        }
+        else
+        {
+            cout << "You got it wrong!" << endl;
+        }
     }
 }
