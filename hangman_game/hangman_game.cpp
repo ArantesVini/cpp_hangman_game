5a8676a166a92ab5e8e3f012e875d385bca2be7e
ArantesVini <vinicius.daniel02@gmail.com>
2023-09-03 19:02:54 -0300

    start the hangman game in cpp

---
+++ hangman_game/hangman_game.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+
+    const string SECRET_WORD = "MELANCIA";
+    cout << SECRET_WORD << endl;
+
+    bool hanged = false;
+    bool won = false;
+
+    while (hanged == false && won == false)
+    {
+        char guess;
+
+        cin >> guess;
+    }
+}
