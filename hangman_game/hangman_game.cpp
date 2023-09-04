7792abe932e854781fc9d357f7954e777422b26f
ArantesVini <vinicius.daniel02@gmail.com>
2023-09-03 21:36:25 -0300

    showing wrong guess using map and vectors

--- hangman_game/hangman_game.cpp
+++ hangman_game/hangman_game.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
 #include <ctime>
 using namespace std;
 
-const string SECRET_WORD = "MELANCIA";
+const string SECRET_WORD = "WATERMELON";
+
+map<char, bool> right_guesses;
+vector<char> wrong_guesses;
 
 bool right_guess(char guess)
 {
@@ -11,8 +16,6 @@ bool right_guess(char guess)
     {
         if (guess == letter)
             return true;
-        else
-            return false;
     }
     return false;
 }
@@ -20,17 +23,48 @@ bool right_guess(char guess)
 int main()
 {
 
-    cout << SECRET_WORD << endl;
+    cout << "╔══════════════════════════╗" << endl;
+    cout << "║      Welcome to the      ║" << endl;
+    cout << "║       Hangman game       ║" << endl;
+    cout << "╚══════════════════════════╝" << endl;
 
     bool hanged = false;
     bool won = false;
 
     while (hanged == false && won == false)
     {
-        char guess;
+        cout << endl;
 
+        if (wrong_guesses.size() > 0)
+        {
+            cout << "Wrong guesses: ";
+            for (char letter : wrong_guesses)
+            {
+                cout << letter << " ";
+            }
+            cout << endl;
+        }
+
+        for (char letter : SECRET_WORD)
+        {
+            if (right_guesses[letter])
+            {
+                cout << letter << " ";
+            }
+            else
+            {
+                cout << "_ ";
+            }
+        }
+        cout << endl;
+
+        char guess;
+        cout << "Type your guess: ";
         cin >> guess;
         guess = toupper(guess);
+
+        right_guesses[guess] = true;
+
         if (right_guess(guess))
         {
             cout << "You got it right!" << endl;
@@ -38,6 +72,8 @@ int main()
         else
         {
             cout << "You got it wrong!" << endl;
+            wrong_guesses.push_back(guess);
         }
+        cout << endl;
     }
 }
