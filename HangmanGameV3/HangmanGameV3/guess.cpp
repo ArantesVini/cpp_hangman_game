25f3f658946e2bd94d2c32cabe0a0d328866cab6
Vinicius Arantes <vinicius.daniel02@gmail.com>
2023-09-10 12:56:04 -0300

    implement validation of wrong guess
    If a wrong guess is been already made, don't push back in the vector again

--- HangmanGameV3/HangmanGameV3/guess.cpp
+++ HangmanGameV3/HangmanGameV3/guess.cpp
@@ -12,7 +12,7 @@ void guess(std::map<char, bool>& right_guesses, std::vector<char>& wrong_guesses
 
     right_guesses[guess] = true;
 
-    if (!right_guess(guess))
+    if (!right_guess(guess) && std::find(wrong_guesses.begin(), wrong_guesses.end(), guess) == wrong_guesses.end())
     {
         wrong_guesses.push_back(guess);
     }
