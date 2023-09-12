f69f3b93272c37e6cff92015394812dc74af1fdb
Vinicius Arantes <vinicius.daniel02@gmail.com>
2023-09-12 10:05:12 -0300

    initial commit of cpp bank
    cpp-bank it will be used to learn about the OO paradigm in C++

---
+++ cpp-bank/cpp-bank/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-bank
+//
+//  Created by Vinícius Daniel Arantes on 12/09/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
