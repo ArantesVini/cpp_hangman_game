27259604d164029c5b65f328debe7003a0ea28e7
Vinicius Arantes <vinicius.daniel02@gmail.com>
2023-09-12 11:37:47 -0300

    start the initial struct and functions of the bank app

--- cpp-bank/cpp-bank/main.cpp
+++ cpp-bank/cpp-bank/main.cpp
@@ -1,14 +1,57 @@
-//
-//  main.cpp
-//  cpp-bank
-//
-//  Created by Vinícius Daniel Arantes on 12/09/23.
-//
-
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Account
+{
+    string number;
+    string holderDocumentNumber;
+    string holderName;
+    float balance;
+};
+
+
+void withdraw(Account& account, float withdrawValue)
+{
+    if (account.balance < withdrawValue)
+    {
+        cout << "Insufficient funds." << endl;
+        return;
+    }
+    
+    if (withdrawValue <= 0)
+    {
+        cout << "Invalid withdraw value" << endl;
+        return;
+    }
+    account.balance -= withdrawValue;
+}
+
+
+void deposit(Account& account, float depositValue)
+{
+    if (depositValue <= 0)
+    {
+        cout << "Invalid deposit value" << endl;
+        return;
+    }
+    account.balance += depositValue;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    Account myAccount;
+    
+    myAccount.number = "12345-6";
+    myAccount.holderDocumentNumber = "123-456";
+    myAccount.holderName = "Vinicius Arantes";
+    myAccount.balance = 100;
+    
+    cout << myAccount.balance << endl;
+    
+    deposit(myAccount, 500);
+    cout << myAccount.balance << endl;
+    
+    withdraw(myAccount, 80);
+    cout << myAccount.balance << endl;
 }
